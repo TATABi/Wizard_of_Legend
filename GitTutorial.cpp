8edc7755563b26f67c0cc89a86ad3f38696a2c82
yansheng0430 <dear90136@gmail.com>
2019-03-06 19:58:00 +0800

    hello 8 yee

--- GitTutorial.cpp
+++ GitTutorial.cpp
@@ -7,16 +7,15 @@ static int target = -1;
 if (target == -1) {
 Random r;
 target = r.Next() % 100 + 1;
-}
-if (number == target)return true;
-return false;
-}
-else if (number < target) {
-std::cout << "Bigger" << std::endl;
-return false;
-}
+if (number == target) {
+std::cout << "Correct !!";
+target = -1;
 return true;
 }
+else std::cout << "Wrong" << std::endl;
+return false;
+}
+
 int main(array<System::String ^> ^args)
 {
 int guess;
