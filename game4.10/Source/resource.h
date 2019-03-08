07f553fee7108374ce1cb629be34dd6e25b3e1a9
yansheng0430 <dear90136@gmail.com>
2019-03-08 11:16:05 +0800

    修正圖檔路徑

--- game4.10/Source/resource.h
+++ game4.10/Source/resource.h
@@ -53,7 +53,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
-#define _APS_NEXT_RESOURCE_VALUE        163
+#define _APS_NEXT_RESOURCE_VALUE        165
 #define _APS_NEXT_COMMAND_VALUE         32776
 #define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
